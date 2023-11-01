4eafedeea4766f49616593e13e8ed98416fe4456
Olivia Jennings <oejennings@users.noreply.github.com>
2023-11-01 12:40:34 -0400

    added ability of users to type name

--- components/Start.js
+++ components/Start.js
@@ -1,12 +1,20 @@
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
 
 const Start = ({ navigation }) => {
+    const [name, setName] = useState('');
  return (
    <View style={styles.container}>
      <Text>Hello Start!</Text>
+     <TextInput
+        style={styles.textInput}
+        value={name}
+        onChangeText={setName}
+        placeholder='Type your username here'
+      />
      <Button
        title="Go to Chat"
-       onPress={() => navigation.navigate('Chat')}
+       onPress={() => navigation.navigate('Chat', { name: name})}
      />
    </View>
  );
@@ -17,7 +25,14 @@ const styles = StyleSheet.create({
    flex: 1,
    justifyContent: 'center',
    alignItems: 'center'
- }
+ },
+ textInput: {
+    width: "88%",
+    padding: 15,
+    borderWidth: 1,
+    marginTop: 15,
+    marginBottom: 15
+  }
 });
 
 export default Start;
