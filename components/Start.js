9d3e69ea59f6076f84c5882de4e4871569b33c2b
Olivia Jennings <oejennings@users.noreply.github.com>
2023-11-01 14:02:33 -0400

    added styling

--- components/Start.js
+++ components/Start.js
@@ -1,19 +1,21 @@
 import { useState } from 'react';
-import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
+import { StyleSheet, View, Text, Button, TextInput, TouchableOpacity } from 'react-native';
 
 const Start = ({ navigation }) => {
     const [name, setName] = useState('');
  return (
+   
    <View style={styles.container}>
-     <Text>Hello Start!</Text>
+     <Text style={styles.title}>Chat App</Text>
      <TextInput
         style={styles.textInput}
         value={name}
         onChangeText={setName}
-        placeholder='Type your username here'
+        placeholder='Your Name'
       />
-     <Button
-       title="Go to Chat"
+      <Text style={styles.text}>Choose you background color:</Text>
+     <TouchableOpacity
+       title="Start Chatting"
        onPress={() => navigation.navigate('Chat', { name: name})}
      />
    </View>
@@ -26,12 +28,31 @@ const styles = StyleSheet.create({
    justifyContent: 'center',
    alignItems: 'center'
  },
+ title: {
+    fontSize: 45, 
+    fontWeight: '600',
+    color: '#FFFFFF'
+ },
  textInput: {
     width: "88%",
     padding: 15,
     borderWidth: 1,
     marginTop: 15,
-    marginBottom: 15
+    marginBottom: 15, 
+    fontSize: 16, 
+    fontWeight: '600', 
+    color: '#757083', 
+
+  },
+  text: {
+    fontSize: 16, 
+    fontWeight: '300', 
+    color: '#757083'
+  }, 
+  button: {
+    fontSize: 16, 
+    fontWeight:'600', 
+    color: '#FFFFFF' 
   }
 });
 
