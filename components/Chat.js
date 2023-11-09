fdb00d9facec8a62140750f7a2cd7625b8d45456
Olivia Jennings <oejennings@users.noreply.github.com>
2023-11-09 11:49:58 -0500

    updated useEffect
    fetch messages
    and updated onSend function

--- components/Chat.js
+++ components/Chat.js
@@ -1,39 +1,35 @@
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 import { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, KeyboardAvoidingView, Platform } from 'react-native';
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc, onSnapshot, orderBy, query } from "firebase/firestore";
 
-const Chat = ({ route, navigation }) => {
+const Chat = ({ route, navigation, db }) => {
     const { name, color } = route.params;
     const [messages, setMessages] = useState([]);
     const onSend = (newMessages) => {
-        setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
-    }
+        addDoc(collection(db, "messages"), newMessages[0])
+      }
    
     useEffect(() => {
-        setMessages([
-            {
-                _id: 1,
-                text: "Hello developer",
-                createdAt: new Date(),
-                user: {
-                  _id: 2,
-                  name: "React Native",
-                  avatar: "https://placeimg.com/140/140/any",
-                }, 
-            },
-            {
-                _id: 2,
-                text: 'You have entered the chat',
-                createdAt: new Date(),
-                system: true,
-              },
-        ]);
+        navigation.setOptions({ title: name });
+        const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+        const unsubMessages = onSnapshot(q, (docs) => {
+          let newMessages = [];
+          docs.forEach(doc => {
+            newMessages.push({
+              id: doc.id,
+              ...doc.data(),
+              createdAt: new Date(doc.data().createdAt.toMillis())
+            })
+          })
+          setMessages(newMessages);
+        })
+        return () => {
+          if (unsubMessages) unsubMessages();
+        }  
     }, []);
 
-    useEffect(() => {
-        navigation.setOptions({ title: name })
-    }, []);
+   
     
     const renderBubble = (props) => {
         return <Bubble
