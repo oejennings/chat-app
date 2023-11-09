1a06e2aa3043e2153ae134404cac370568cc9957
Olivia Jennings <oejennings@users.noreply.github.com>
2023-11-09 11:25:31 -0500

    imported Firestore
    added database initialization code

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import Start from './components/Start';
 import Chat from './components/Chat';
 
@@ -9,6 +11,22 @@ import Chat from './components/Chat';
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBvdv7AcrafJ0cBOsMB4KC6Nnh1KTQMY20",
+    authDomain: "chat-app-6c201.firebaseapp.com",
+    projectId: "chat-app-6c201",
+    storageBucket: "chat-app-6c201.appspot.com",
+    messagingSenderId: "544070228859",
+    appId: "1:544070228859:web:8d5a055579eacb0b0fbbde"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+  
   return (
     <NavigationContainer>
        <Stack.Navigator
