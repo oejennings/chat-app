d2532b45c1800d961306a85ffcb9a36a07dd6b7a
Olivia Jennings <oejennings@users.noreply.github.com>
2023-11-14 11:27:06 -0500

    installed NetInfo

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { useNetInfo }from '@react-native-community/netinfo';
 import Start from './components/Start';
 import Chat from './components/Chat';
 
