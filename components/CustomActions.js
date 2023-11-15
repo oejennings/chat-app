af7e080d0ea932c6ab0e5f17f161a2d1fa0b96c9
Olivia Jennings <oejennings@users.noreply.github.com>
2023-11-15 11:20:21 -0500

    created pickImage function

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,1 +1,26 @@
 import * as ImagePicker from 'expo-image-picker';
+import { StyleSheet } from 'react-native';
+
+const CustomActions = ({}) => {
+
+    const pickImage = async () => {
+        let permissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    
+        if (permissions?.granted) {
+           let result = await ImagePicker.launchImageLibraryAsync();
+    
+          if (!result.canceled) setImage(result.assets[0]);
+          else setImage(null)
+        }
+      }
+    
+    return (
+
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default CustomActions;
